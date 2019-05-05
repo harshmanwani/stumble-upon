7d159e442d48639205b346883e6690f6a6066a86
Harsh <invokerharsh@gmail.com>
2019-05-05 22:18:34 +0530

    added routing in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import logo from '../logo.svg';
+import { Route, Switch } from 'react-router-dom'
+import Header from './Header';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 
@@ -8,12 +9,16 @@ import '../styles/App.css';
 class App extends Component {
   render(){
     return (
-      <div>
-        <LinkList/>
-        <CreateLink/>
+      <div className="center w85">
+        <Header/>
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList}/>
+            <Route exact path="/create" component={CreateLink}/>
+          </Switch>
+        </div>
       </div>
     ) 
-
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
@@ -20,9 +21,11 @@ const client = new ApolloClient({
 
 
 ReactDOM.render(
-	<ApolloProvider client={client}>
-		<App />	
-	</ApolloProvider>,
+	<BrowserRouter>
+		<ApolloProvider client={client}>
+			<App />	
+		</ApolloProvider>
+	</BrowserRouter>,
 	document.getElementById('root')
 );
 
