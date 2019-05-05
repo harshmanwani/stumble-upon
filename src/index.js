899e2e7eb6a18875c820cbbc343e5601320e54e3
Harsh <invokerharsh@gmail.com>
2019-05-06 01:40:47 +0530

    authorization token middleware with apollo link context

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
+import { AUTH_TOKEN } from './constants';
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 
@@ -14,8 +16,18 @@ const httpLink = createHttpLink({
 	uri: "http://localhost:4000"
 })
 
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN)
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ''
+		}
+	}
+})
+
 const client = new ApolloClient({
-	link: httpLink,
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache()
 })
 
