538fef4e63e31cc35f10ee5a5d3b36e9deee78ea
Harsh <invokerharsh@gmail.com>
2019-05-06 03:33:04 +0530

    wrote feed-search-query

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,31 @@ import { withApollo } from 'react-apollo';
 import gql from 'graphql-tag';
 import Link from './Link';
 
+const FEED_SEARCH_QUERY = gql`
+	query FeedSearchQuery($filter: String!) {
+		feed(filter: $filter) {
+			links {
+				id
+				url
+				description
+				createdAt
+				postedBy {
+					id
+					name
+				}
+				votes {
+					id
+					user {
+						id
+					}
+				}
+			}
+		}
+	}
+`
+
+
+
 class Search extends Component {
 	state = {
 		links: [],
