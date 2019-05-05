a6aebb563482d5c2fce7e9d5443a12662ea05809
Harsh <invokerharsh@gmail.com>
2019-05-06 01:56:18 +0530

    modified link to show votes, user and time of post

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { AUTH_TOKEN } from '../constants'
+import { timeDifferenceForDate } from '../utils' 
 
-const Link = ({link}) => {
+const Link = (props) => {
+
+	const _voteForLink = () => {
+		//
+	}
+
 	return (
-		<div>
-			<div>
-				{link.description} ({link.url})
+		<div className="flex mt2 items-start">
+			<div className="flex items-center">
+				<span className="gray">{props.index + 1}.</span>
+				{AUTH_TOKEN && (
+					<div className="ml1 gray f11" onClick={_voteForLink}>
+						▲
+					</div>
+				)}
+			</div>
+			<div className="ml1">
+				<div>
+					{props.link.description} ({props.link.url})	
+				</div>
+				<div className="f6 lh-opy gray">
+					{props.link.votes.length} votes | by{' '}
+					{props.link.postedBy
+					  ? props.link.postedBy.name
+					  : 'Unknown'
+					}
+					&nbsp;
+					{timeDifferenceForDate(props.link.createdAt)}
+				</div>
 			</div>
 		</div>
 	)
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -30,7 +30,7 @@ const LinkList = () => {
 				return (
 					<div>
 						{
-							linksToRender.map(link => <Link key={link.id} link={link}/>)
+							linksToRender.map((link, index) => <Link key={link.id} link={link} index={index}/>)
 						}
 					</div>
 				)}
