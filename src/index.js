c3c4fcd01a690f1b3087b85f07d3a2f8c0c63e54
Harsh <invokerharsh@gmail.com>
2019-05-25 18:05:19 +0530

    websocket link handle in apollo-link

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
 const httpLink = createHttpLink({
@@ -26,8 +29,31 @@ const authLink = setContext((_, { headers }) => {
 	}
 })
 
+const wsLink = new WebSocketLink({
+	uri: `ws://localhost:4000`,
+	options: {
+		reconnect: true,
+		connectionParams: {
+			authToken: localStorage.getItem(AUTH_TOKEN),
+		}
+	}
+})
+
+// using split for proper 'routing' of the requests. between socket and normal requests.
+// the first argument which is a test function returns a boolean.
+// if true, the request will be forwarded to the link passed in the second argument (wsLink). else to the third one.
+// Hybrid Link -> Apollo-Link -> (Query/Mutation -> Http-Link) ||  (Subscriptions -> WebSocket-Link )
+const link = split(
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query)
+		return kind === 'OperationDefinition' && operation === 'subscription'
+	},
+	wsLink,
+	authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-	link: authLink.concat(httpLink),
+	link,
 	cache: new InMemoryCache()
 })
 
