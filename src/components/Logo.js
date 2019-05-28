067da589accce9d923285be8a0173e14e380b731
Harsh <invokerharsh@gmail.com>
2019-05-28 23:11:20 +0530

    created logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/logo.sass';
+
+const Logo = () => (
+    <div className="logo">
+        <div className="logo-container">
+            <div className="square square-one face-top"></div>
+            <div className="square square-two face-top"></div>
+            <div className="square square-three face-top"></div>
+            <div className="square square-four side"></div>
+            <div className="square square-five side-2"></div>
+        </div>
+    </div>
+)
+
+export const Logo;
